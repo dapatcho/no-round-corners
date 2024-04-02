1094860abce56ea837476439b8f120dba2abf3ae
dapatcho <boulosmk@gmail.com>
2024-04-02 15:20:05 -0400

    Next.js installed, work towards actual website content now.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+<div className="navbar">
+  <div className="nav-title">
+    <h2>michael boulos</h2>
+  </div>
+  <div className="nav-content">
+    <a>about</a>
+    <a>aortfolio</a>
+    <a>extracurriculars</a>
+    <a>resume</a>
+  </div>
+</div>
