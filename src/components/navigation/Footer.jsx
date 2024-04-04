28a5a0a037e769911254e14f0e760736f60b9ed4
dapatcho <boulosmk@gmail.com>
2024-04-04 13:15:25 -0400

    Work on footer CSS

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -5,18 +5,22 @@ import Link from "next/link";
 function Footer() {
   return (
     <div className={styles["footer"]}>
-      <div className="right">
-        <div className="stripe"></div>
-        <div className="tagline"><p>websites, engineered</p></div>
-      </div>
-      <div className="left">
-        <div className="linkedin">
+      <div className={styles["right"]}>
+        <div className={styles["linkedin"]}>
           <Link href="https://www.linkedin.com/in/boulosm/">
             linkedin: michael boulos
           </Link>
         </div>
-        <div className="email">
+        <div className={styles["email"]}>
           <p>boulosmk@gmail.com</p>
+        </div>
+      </div>
+      <div className={styles["left"]}>
+        <div className={styles["stripe"]}>
+          <div></div>
+        </div>
+        <div className={styles["tagline"]}>
+          <p>websites, engineered</p>
         </div>
       </div>
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/navigation/Navbar";
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+      className={`flex min-h-screen flex-col items-center justify-between p-4 `}
     >
       <p>
         Hey, I’m Michael (he/they), web developer and civil engineering
