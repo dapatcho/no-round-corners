b655ec2c4aeceac02193eff7b08608351b97c828
Michael Boulos <boulosmk@gmail.com>
2024-04-04 10:53:49 -0400

    Fonts updated, did not figure out how to use the preferred "optimization" in the next JS docs.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,17 +3,6 @@ import "@/styles/globals.css";
 import Navbar from "@/components/navigation/Navbar";
 import Footer from "@/components/navigation/Footer";
 
-import { google } from "next/font/google";
-
-const firaCode = google({
-  family: "Fira Code",
-  weight: ["100", "400", "700"], // Add the weights you need
-});
-
-const firaSans = google({
-  family: "Fira Sans",
-  weight: ["700"], // Add the weights you need
-});
 
 export default function App({ Component, pageProps }) {
   return (
