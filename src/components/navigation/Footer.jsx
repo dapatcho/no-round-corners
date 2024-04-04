8a639f40f1ed1e19f588ed1dead7cd02f7a3c769
Michael Boulos <boulosmk@gmail.com>
2024-04-04 11:45:50 -0400

    Learning how to use and make use of CSS Modules

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import styles from "./Footer.module.css";
 import Link from "next/link";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-right">
-        <div className="footer-stripe"></div>
-        <div className="footer-tagline">websites, engineered</div>
+    <div className={styles["footer"]}>
+      <div className="right">
+        <div className="stripe"></div>
+        <div className="tagline"><p>websites, engineered</p></div>
       </div>
-      <div className="footer-left">
-        <div className="footer-linkedin">
+      <div className="left">
+        <div className="linkedin">
           <Link href="https://www.linkedin.com/in/boulosm/">
             linkedin: michael boulos
           </Link>
         </div>
-        <div className="footer-email">
+        <div className="email">
           <p>boulosmk@gmail.com</p>
         </div>
       </div>
