ce5cde42afa3cb33e799d71ea73448fb28eb1183
Michael Boulos <boulosmk@gmail.com>
2024-04-26 17:25:19 -0400

    Fixed FOUC through a loading state

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,13 +4,42 @@ import Layout from "@/components/Layout";
 import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a full page load.
+    // You might want to replace this with actual data fetching logic.
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 50); // adjust time based on your needs
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme-initialized", true);
+  }, []);
+
   return (
-    <ThemeProvider>
-      <Layout>
-        <div>
-          <Component {...pageProps} />
+    <>
+      <ThemeProvider
+        attribute="data-theme"
+        enableSystem={true}
+        defaultTheme="system"
+      >
+        {" "}
+        <div
+          style={{
+            opacity: isLoading ? 0 : 1,
+            transition: "opacity 0.5s ease-in",
+          }}
+        >
+          {" "}
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </div>
-      </Layout>
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   );
 }
