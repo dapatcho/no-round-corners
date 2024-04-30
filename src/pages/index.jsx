d938b052d85d67ef058d7fd5fbe3f68b3a6a981b
Mikey <112041318+dapatcho@users.noreply.github.com>
2024-04-29 20:19:43 -0400

    Redoing index layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,20 +8,21 @@ export default function Home() {
     <div className={styles.parent}>
       <div className={styles.spacer}></div>
       <div className={styles.content}>
-        <p className="">
+        <p className={styles.blurb}>
           Hey, I’m Michael, web developer and civil engineer, as well as radio
           show host, DIY music organizer, performer, and graphic designer. I
           love music, have an obsession with coffee, and clearly wear too many
           hats.
         </p>
-      </div>
-      <div className={styles.headshot}>
+        <div className={styles.headshot}>
         <img
           src="/Headshot.png"
           alt="Headshot of Michael Boulos, web developer"
 
         />
       </div>
+      </div>
+
     </div>
   );
 }
