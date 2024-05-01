57f3d8c727c1cba1827d23e5a713a7809a6cd860
Mikey <112041318+dapatcho@users.noreply.github.com>
2024-05-01 18:31:20 -0400

    Fixed word salad

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -11,7 +11,7 @@ export default function portfolio() {
         link="https://www.civiconnect.ca/"
         imgUrlDark="/civi-dark.png"
         imgUrlLight="/civi-light.png"
-        bodyText={`-During my four month internship I was part of the three person team developing the redesigned Civiconnect website redesign from the ground up, making a dynamic and mobile friendly device with Node.js and Strapi CMS integration. \n -Designed and implemented engaging and responsive website components which.\n-Components also had extensive integration with Strapi CMS to provide the end users with autonomy and a practical means of maintaining and updating content throughout the site.`}
+        bodyText={`-During my four month internship I was developing the Civiconnect website redesign from the ground up in a team of three. \n -I designed and implemented engaging and responsive website components.\n-Components also had extensive integration with Strapi CMS to provide the end users with autonomy and a practical means of maintaining and updating content throughout the site.`}
       />{" "}
       <BufferBar accentColor="var(--accent)" />
       <PortfolioItem
