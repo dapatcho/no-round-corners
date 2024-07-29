687a8acd28f076d4c89b912f6ba54ed6979e41d6
Mikey <112041318+dapatcho@users.noreply.github.com>
2024-07-29 17:48:44 -0400

    Further style tweaks made.

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -11,13 +11,13 @@ export default function portfolio() {
         link="https://www.civiconnect.ca/"
         imgUrlDark="/civi-dark.png"
         imgUrlLight="/civi-light.png"
-        bodyText={`-During my four month internship I was developing the Civiconnect website redesign from the ground up in a team of three. \n -I designed and implemented engaging and responsive website components.\n-Components also had extensive integration with Strapi CMS to provide the end users with autonomy and a practical means of maintaining and updating content throughout the site.`}
+        bodyText={`-During my four month internship I was developing the Civiconnect website redesign from the ground up in a team of three. \n-I designed and implemented engaging and responsive website components.\n-Components also had extensive integration with Strapi CMS to provide the end users with autonomy and a practical means of maintaining and updating content throughout the site.`}
       />{" "}
       <BufferBar accentColor="var(--accent)" />
       <PortfolioItem
         title="mboulos.dev"
         link="https://mboulos.dev/"
-        bodyText="My personal website, designed from the ground up in Figma, then written in HTML, CSS, and JavaScript using the React framework."
+        bodyText="-My personal website, designed from the ground up in Figma, then written in HTML, CSS, and JavaScript using the React framework."
       />{" "}
     </div>
   );
